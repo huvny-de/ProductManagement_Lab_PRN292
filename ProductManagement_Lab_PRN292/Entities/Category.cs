8086642dc8b866b2e7e9d5c9ef82697c75287ede
Huy Vo <neo.pinhuy00@gmail.com>
2021-06-28 10:36:10 +0700

    Dbcontexts

--- ProductManagement_Lab_PRN292/Entities/Category.cs
+++ ProductManagement_Lab_PRN292/Entities/Category.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProductManagement_Lab_PRN292.DTO
+namespace ProductManagement_Lab_PRN292.Entities
 {
     public class Category
     {
