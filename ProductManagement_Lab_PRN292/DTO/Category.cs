54534ae44e3dadf237e22d41351955d05edd1782
Huy Vo <neo.pinhuy00@gmail.com>
2021-06-28 10:00:14 +0700

    DTO

---
+++ ProductManagement_Lab_PRN292/DTO/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductManagement_Lab_PRN292.DTO
+{
+    public class Category
+    {
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set }
+    }
+}
---
+++ ProductManagement_Lab_PRN292/DTO/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductManagement_Lab_PRN292.DTO
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public int Amount { get; set; }
+
+        public string ProductInCategory { get; set; }
+
+    }
+}
