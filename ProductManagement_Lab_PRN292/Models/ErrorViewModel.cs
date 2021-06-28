fea324526548c4ff065387ccf0b22ef4aac8c4ff
Huy Vo <neo.pinhuy00@gmail.com>
2021-06-28 09:53:50 +0700

    asd

---
+++ ProductManagement_Lab_PRN292/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductManagement_Lab_PRN292.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
