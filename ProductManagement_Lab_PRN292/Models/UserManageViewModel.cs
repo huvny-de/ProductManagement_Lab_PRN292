6cf24e325989ec02876a160fee8548d460bda52d
Huy Vo <neo.pinhuy00@gmail.com>
2021-07-01 22:07:47 +0700

    UserManageViewModel

---
+++ ProductManagement_Lab_PRN292/Models/UserManageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductManagement_Lab_PRN292.Models
+{
+    public class UserManageViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Role { get; set; }
+
+    }
+}
